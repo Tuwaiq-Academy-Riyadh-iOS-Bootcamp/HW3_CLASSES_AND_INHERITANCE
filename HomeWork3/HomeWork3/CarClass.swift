fa756949833a6b9bd38554696ad1c599e62fb028
= <=>
2021-10-20 20:20:13 +0300

    uploading home work 3

---
+++ HomeWork3/HomeWork3/CarClass.swift
@@ -0,0 +1,12 @@
+//
+//  CarClass.swift
+//  HomeWork3
+//
+//  Created by Rayan Taj on 20/10/2021.
+//
+
+import Foundation
+
+class Car : Vehicle {
+    
+}
---
+++ HomeWork3/HomeWork3/MotorcycleClass.swift
@@ -0,0 +1,13 @@
+//
+//  MotorcycleClass.swift
+//  HomeWork3
+//
+//  Created by Rayan Taj on 20/10/2021.
+//
+
+import Foundation
+
+
+class Motorcycle : Vehicle {
+    
+}
