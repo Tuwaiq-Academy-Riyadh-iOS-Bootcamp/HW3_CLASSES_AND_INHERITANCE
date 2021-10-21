38e82a1b139f0aafeba2a9a3ccf3d5407d4af8ab
Nora8080 <89574722+Nora8080@users.noreply.github.com>
2021-10-21 12:12:48 +0300

    Nora alluqmani

---
+++ hw3clsses/hw3clsses/Vehicle.swift
@@ -0,0 +1,21 @@
+//
+//  Vehicle.swift
+//  Vehicle
+//
+//  Created by نورا on 19/10/2021.
+//
+
+import Foundation
+
+class Vehicle {
+    
+   var name: String
+   var model :String
+    
+    init(_ name:String , _ model:String) {
+        self.name = name
+        self.model = model
+    }
+    
+}
+
---
+++ hw3clsses/hw3clsses/utils.swift
@@ -0,0 +1,28 @@
+//
+//  utils.swift
+//  utils
+//
+//  Created by نورا on 19/10/2021.
+//
+
+import Foundation
+
+class Utils {
+
+    static func readInt() -> Int {
+        return Int(readLine()!)!
+    }
+    
+    static func readFloat() -> Float {
+        return Float(readLine()!)!
+    }
+    
+    static func readDouble() -> Double {
+        return Double(readLine()!)!
+    }
+    
+    static func readString() -> String {
+        return readLine()!
+    }
+
+}
